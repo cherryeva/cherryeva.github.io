642b8eb6934441e887b8113528829790cf583849
cherryeva <nekmakarek@gmail.com>
2024-12-30 05:06:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const heart = document.querySelector('.heart-animation');
+    heart.addEventListener('click', () => {
+        alert('Ты прекрасна! С днем рождения!');
+    });
+});
